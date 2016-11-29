1c58d58f18a56c12762ec1870f180d992f660582
Ivan Eisenberg <ivan.eisenberg@gmail.com>
2016-11-29 14:43:20 -0600

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # apt-get packages
-sudo apt-get install build-essential curl git git-core zsh m4 ruby texinfo libbz2-dev libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev
+sudo apt-get install git-core git-flow build-essentials python-dev python-  pip
+curl https://raw.githubusercontent.com/creationix/nvm/v0.20.0/install.sh | bash
+npm config set prefix ~/.local/share/npm
+nvm install stable
+nvm alias default stable
 
 # Install Oh-My-ZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -16,9 +20,6 @@ echo 'export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"' >> ~/.zshrc
 echo 'export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"' >> ~/.zshrc
 
 brew update
-
-# Install node.js
-brew install node
 
 # Install Atom code editor
 sudo add-apt-repository ppa:webupd8team/atom
